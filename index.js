7a60d17c8cea5487d92c44b20554a7cb361c5aa4
irminabartek <irmina.bartek@gmail.com>
2018-02-04 22:58:02 +0000

    real pin toggle

--- index.js
+++ index.js
@@ -7,20 +7,14 @@ var Gpio = require('onoff').Gpio;
 var port = 3000;
 var host = '0.0.0.0';
 
-function toggleGpioPin(mode, pinNumber, value){
-    var direction;
-    if(mode === 'write'){
-        direction = 'out';
-    }else if(mode === 'read'){
-        direction = 'in';
-    }else{
-        throw new Error('Unsupported GPIO mode');
-    }
-    var gpioPin = new Gpio(pinNumber, direction);
+function toggleGpioPin(pinNumber){
+    var gpioPin = new Gpio(pinNumber, "out");
     if (gpioPin.readSync() === 0) {
-        gpioPin.writeSync(value);
+        gpioPin.writeSync(1);
+        return 1;
     } else {
         gpioPin.writeSync(0);
+        return 0;
     }
 }
 
@@ -29,13 +23,12 @@ server.connection({port: port, host: host, routes: {cors: true}});
 
 server.route({
     method: 'GET',
-    path: '/api/gpio/{mode}/{pin}/{value}',
+    path: '/api/gpio/{pin}',
     handler: function(request, reply){
-        var mode = encodeURIComponent(request.params.mode);
         var pin = encodeURIComponent(request.params.pin);
-        var value = parseInt(encodeURIComponent(request.params.value));
-	    toggleGpioPin(mode, pin, value);
-        reply({mode: mode, pin: pin}).code(200);
+        console.log("gpio endpoint called with pin number ", pin);
+        var result = toggleGpioPin(pin);
+        reply({value: result}).code(200);
     }
 });
 
@@ -44,5 +37,5 @@ server.start(function(err){
         throw err;
     }
 
-    console.log("Server started at "+port);
+    console.log("Server started at " + port);
 });
