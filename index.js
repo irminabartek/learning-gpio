b20f9427b3980b4d40800fe7857b5e37fd601eef
jbeynar <jakub.bejnarowicz@gmail.com>
2018-02-05 20:44:58 +0000

    gpio toggle to write

--- index.js
+++ index.js
@@ -7,35 +7,30 @@ var Gpio = require('onoff').Gpio;
 var port = 3000;
 var host = '0.0.0.0';
 
-function toggleGpioPin(pinNumber){
-    var gpioPin = new Gpio(pinNumber, "read");
-    if (gpioPin.readSync() === 0) {
-        gpioPin.writeSync(1);
-        return 1;
-    } else {
-        gpioPin.writeSync(0);
-        return 0;
-    }
+function setGpioValue(pinNumber, value) {
+  var gpioPin = new Gpio(pinNumber, "out");
+  return gpioPin.writeSync(value);
 }
 
 var server = new Hapi.Server();
 server.connection({port: port, host: host, routes: {cors: true}});
 
 server.route({
-    method: 'GET',
-    path: '/api/gpio/{pin}',
-    handler: function(request, reply){
-        var pin = encodeURIComponent(request.params.pin);
-        console.log("GPIO endpoint called with pin number ", pin);
-        var result = toggleGpioPin(pin);
-        reply({value: result}).code(200);
-    }
+  method: 'GET',
+  path: '/api/gpio/{pin}/{value}',
+  handler: function (request, reply) {
+    var pin = encodeURIComponent(request.params.pin);
+    var value = encodeURIComponent(request.params.value);
+    console.log("GPIO endpoint called with pin number ", pin);
+    var result = setGpioValue(pin, value);
+    reply({ value: result }).code(200);
+  }
 });
 
-server.start(function(err){
-    if(err){
-        throw err;
-    }
+server.start(function (err) {
+  if (err) {
+    throw err;
+  }
 
-    console.log("Server started at " + port);
+  console.log("Server started at " + port);
 });
